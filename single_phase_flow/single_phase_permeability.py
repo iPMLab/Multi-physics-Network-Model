9a94bd822d882d4109a3d0e0c713037fd7e7dcae
MingLiang-Qu <62739163+MingLiang-Qu@users.noreply.github.com>
2024-01-15 11:43:13 +0000

    Update single_phase_permeability.py
    The file's name has been changed.

--- single_phase_flow/single_phase_permeability.py
+++ single_phase_flow/single_phase_permeability.py
@@ -6,9 +6,9 @@ Created on Tue Apr 26 21:22:24 2022
 @author: htmt
 """
 
-from MpNM._network import network
-from MpNM._topotools import topotools
-from MpNM._algorithm import algorithm
+from MpNM.network import network
+from MpNM.topotools import topotools
+from MpNM.algorithm import algorithm
 import numpy as np
 import argparse  
 import time
