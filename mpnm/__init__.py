6a2d7a667f0fb4227381bfcf46f178552ba65131
yojeep <1379690602@qq.com>
2024-08-28 14:56:55 +0800

    Update __init__.py

--- mpnm/__init__.py
+++ mpnm/__init__.py
@@ -3,10 +3,11 @@ This package contains the classes and functions necessary to run the mpnm algori
 '''
 
 
-
+import numpy as np
 from mpnm._algorithm import algorithm
 from mpnm._Base import Base
 from mpnm._network import network
 from mpnm._topotools import topotools
 from mpnm._phase import phase
+np.seterr(divide='ignore', invalid='ignore')
 # __all__=['_algorithm','_Base','_network','_phase','_topotools']
